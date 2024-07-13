3f2d740a488e3e90ac531d726972566c2ba36678
jekkos <jeroen.peelaerts@gmail.com>
2024-07-13 23:37:20 +0200

    Calculate balances endpoint working

--- backend/src/app/httpserver.py
+++ backend/src/app/httpserver.py
@@ -1,4 +1,6 @@
+import asyncio
 import json
+from dataclasses import asdict
 from datetime import datetime, timezone
 
 from fastapi import FastAPI
@@ -7,7 +9,7 @@ from fastapi.responses import JSONResponse
 from app import repository
 from app import settlement
 from models.community import Community
-from models.meter import ean541449500000446547
+from models.meter import ean541448820060527996
 from models.meterdata import MeterData
 from models.timeperiod import TimePeriod, genesis
 
@@ -26,14 +28,16 @@ async def store_ean(mapping: repository.EanMapping) -> None:
 async def calculate_balances(ean: str, start: datetime, end: datetime):
     time_period = TimePeriod(start, end)
     settlement_result = await settlement.settle(time_period, Community.create())
-    dumps = json.dumps(settlement_result, default=str, sort_keys=True)
-    return JSONResponse(content=dumps, media_type="application/json")
+    results = {ean: {"amount_paid": result.amount_paid, "amount_earned": result.amount_earned} for result in
+               settlement_result.results if ean in result.eans}
+    json_response = json.dumps(results, default=str, sort_keys=True)
+    return JSONResponse(content=json.loads(json_response), media_type="application/json")
 
 
 @app.get("/load_meter_data")
 async def load_meter_data(ean: str, start: datetime, end: datetime):
     # just serve a stub for now, EAN might not be known in frontend
-    meter_data = MeterData.from_file(ean541449500000446547, TimePeriod(start, end))
+    meter_data = MeterData.from_file(ean, TimePeriod(start, end))
 
     json_response = meter_data.df_kwh.to_json(force_ascii=True)
     return JSONResponse(content=json.loads(json_response), media_type="application/json")
@@ -42,8 +46,10 @@ async def load_meter_data(ean: str, start: datetime, end: datetime):
 if __name__ == "__main__":
     start = genesis
     end = datetime.now(timezone.utc)
-    meter_data: MeterData = MeterData.from_file(ean541449500000446547, TimePeriod(start, end))
-    ja =(meter_data.df_kwh / 1000).to_json()
-    df_json = json.loads(ja)
+    time_period = TimePeriod(start, end)
+    settlement_result = asyncio.run(settlement.settle(time_period, Community.create()))
+    results = [asdict(result) for result in settlement_result.results if ean541448820060527996 in result.eans]
+    ean = ean541448820060527996
+    json_response = json.dumps(results, default=str, sort_keys=True)
 
 
--- backend/src/models/settlement.py
+++ backend/src/models/settlement.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 
 from models.community import CommunityId
+from models.meter import EAN
 from models.participant import ParticipantId
 from models.timeperiod import TimePeriod
 
 
 @dataclass(frozen=True)
 class ParticipantSettlement:
+    eans: list[EAN]
     participant_id: ParticipantId
     amount_paid: int
     amount_earned: int
