afb5c078f8227fe304e208663149e796c7d8fd42
Boris Smidt <smidtboris@gmail.com>
2024-07-13 13:10:59 +0200

    correct api

--- backend/src/main.py
+++ backend/src/main.py
@@ -13,21 +13,17 @@ ParticipantId=int
 CommunityId=int
 
 @dataclasses.dataclass
-class Community:
-    participantId: int
-
-@dataclasses.dataclass
-class Participants:
+class Participant:
     id: int
     active: bool
     meters: list[Meter]
 @dataclasses.dataclass
-class SettlmentRequest:
-    community: Community
-    participants: list[Participants]
+class Community:
+    participantId: int
+    participants: list[Participant]
 
 @dataclasses.dataclass
-class ParticipantResult:
+class SettlementResult:
     participantId: ParticipantId
     value: int
 
@@ -36,6 +32,27 @@ class SettlementResult:
     community: int
     result: list[ParticipantResult]
 
-async def settle(request: SettlmentRequest) -> SettlementResult:
+async def settle(community: Community) -> SettlementResult:
     # todo add the settlement magic here!!!
     return SettlementResult(1, [])
+
+def getCommunities() -> list[Community]:
+    # get the community from the smart contract.
+    return []
+
+def updateContract(result : SettlementResult) -> None:
+    # todo write this back to the contract
+    return None
+
+
+async def run() -> None:
+    communities = getCommunities()
+
+    for c in communities:
+        result = await settle(c)
+        updateContract(result)
+
+
+
+if __name__ == '__main__':
+    run()
