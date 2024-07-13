3763228c05703000106fa76dbdd4cd04271e2bfc
Boris Smidt <smidtboris@gmail.com>
2024-07-13 12:56:50 +0200

    add the settlment api

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,35 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+Meter=str
+ParticipantId=int
+CommunityId=int
+
+@dataclasses.dataclass
+class Community:
+    participantId: int
+
+@dataclasses.dataclass
+class Participants:
+    id: int
+    active: int
+    meters: list[Meter]
+@dataclasses.dataclass
+class SettlmentRequest:
+    community: Community
+    participants: list[Participants]
+
+@dataclasses.dataclass
+class ParticipantResult:
+    participantId: ParticipantId
+    value: int
+
+@dataclasses.dataclass
+class SettlementResult:
+    community: int
+    result: list[ParticipantResult]
+
+async def settle(request: SettlmentRequest) -> SettlementResult:
+    # todo add the settlement magic here!!!
+    return SettlementResult(1, [])
